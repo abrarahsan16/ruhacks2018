7a3a3829305b313b7a5bc57b34a91d1a4105f674
Sujan Kandeepan <kandeeps@mcmaster.ca>
2018-05-26 08:24:17 -0400

    Added button to move to authentication screen

--- app/src/main/java/helloworld/abrarahsan/myapplication/MainActivity.java
+++ app/src/main/java/helloworld/abrarahsan/myapplication/MainActivity.java
@@ -1,13 +1,25 @@
 package helloworld.abrarahsan.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button continueButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        continueButton = findViewById(R.id.continueButton);
+    }
+
+    public void continuePressed(View view) {
+        Intent intent = new Intent(getApplicationContext(), AuthenticationActivity.class);
+        startActivity(intent);
     }
 }
