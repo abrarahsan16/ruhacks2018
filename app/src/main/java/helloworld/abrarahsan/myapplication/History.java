35fe7bf8ca065d4010c5bb39e5206e8e92ec0fe9
Abrar Ahsan <ahsan.abrar16@hotmail.com>
2018-05-26 12:58:59 -0400

    history

--- app/src/main/java/helloworld/abrarahsan/myapplication/History.java
+++ app/src/main/java/helloworld/abrarahsan/myapplication/History.java
@@ -11,9 +11,12 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
@@ -36,19 +39,34 @@ public class History extends AppCompatActivity {
         barchart.setDrawGridBackground(true);
 
         ArrayList<BarEntry> barEntries = new ArrayList<>();
-        barEntries.add(new BarEntry(1, 1000, 400));
-        barEntries.add(new BarEntry(2, 2000, 1000));
-        barEntries.add(new BarEntry(3, 500, 1000));
+        barEntries.add(new BarEntry(1, 1000));
+        barEntries.add(new BarEntry(2, 2000));
+        barEntries.add(new BarEntry(3, 500));
 
-        BarDataSet barDataSet = new BarDataSet(barEntries, "Set1");
+        ArrayList<BarEntry> barEntries2 = new ArrayList<>();
+        barEntries2.add(new BarEntry(1, 400));
+        barEntries2.add(new BarEntry(2, 1000));
+        barEntries2.add(new BarEntry(3, 1000));
+
+        BarDataSet barDataSet = new BarDataSet(barEntries, "Income");
+        BarDataSet barDataSet2 = new BarDataSet(barEntries2, "Expense");
         barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
 
-        BarData data=new BarData(barDataSet);
-        data.setBarWidth(0.8f);
+        BarData data=new BarData(barDataSet, barDataSet2);
+        data.setBarWidth(0.6f);
 
         barchart.setData(data);
+        barchart.groupBars(1980f,0.06f,0.02f);
+        barchart.invalidate();
 
-
+        String[] months = new String[] {"Jan","Feb","Mar"};
+        XAxis xAxis = barchart.getXAxis();
+        xAxis.setValueFormatter(new MyXAxisValueFormatter(months));
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setGranularity(1);
+        xAxis.setCenterAxisLabels(true);
+        xAxis.setAxisMinimum(1);
+        xAxis.setAxisMaximum(12);
         /*String content = "<html>"
                 + "            <head>"
                 + "               <script type = \"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>"
@@ -102,7 +120,16 @@ public class History extends AppCompatActivity {
         }
     }*/
     }
-    
+    public class MyXAxisValueFormatter implements IAxisValueFormatter{
+        private String[] mValues;
+        public MyXAxisValueFormatter(String [] values){
+            this.mValues=values;
+        }
+        @Override
+        public String getFormattedValue(float value, AxisBase axis) {
+            return mValues[(int)value];
+        }
+    }
 }
 
 
