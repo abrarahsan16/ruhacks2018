ed0ee54512372fd5c245937c680a5c0dd5b75695
Sujan Kandeepan <kandeeps@mcmaster.ca>
2018-05-27 08:44:24 -0400

    Fixed history data refresh

--- app/src/main/java/helloworld/abrarahsan/myapplication/HistoryActivity.java
+++ app/src/main/java/helloworld/abrarahsan/myapplication/HistoryActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.BarChart;
@@ -25,11 +26,11 @@ import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
 public class HistoryActivity extends Activity {
-    BarChart barchart;
     public static boolean isRecursionEnable = false;
 
     @Override
@@ -38,26 +39,10 @@ public class HistoryActivity extends Activity {
         setContentView(R.layout.activity_history);
         //runInBackground();
 
-        barchart = (BarChart) findViewById(R.id.barchart);
-
-        barchart.setDrawBarShadow(false);
-        barchart.setDrawValueAboveBar(true);
-        barchart.setMaxVisibleValueCount(50);
-        barchart.setPinchZoom(false);
-        barchart.setDrawGridBackground(true);
-        barchart.setGridBackgroundColor(Color.TRANSPARENT);
-
-        /*final double food = new double;
-        final double[] misc = new double[1];
-        final double[] trans = new double[1];
-        final double[] util = new double[1];*/
-        float f, m, t, u;
-
         if (ParseUser.getCurrentUser() != null) {
 
             ParseQuery<ParseUser> query = ParseUser.getQuery();
-            query.whereEqualTo("username", ParseUser.getCurrentUser().getUsername().toString());
-
+            query.whereEqualTo("email", ParseUser.getCurrentUser().getEmail().toString());
             query.findInBackground(new FindCallback<ParseUser>() {
 
 
@@ -65,11 +50,12 @@ public class HistoryActivity extends Activity {
                 public void done(List<ParseUser> objects, ParseException e) {
                     if (e == null && !objects.isEmpty()) {
                         ParseUser user = objects.get(0);
-                        double food, misc, trans, util;
-                        food = user.getDouble("food");
-                        misc = user.getDouble("misc");
-                        trans = user.getDouble("trans");
-                        util = user.getDouble("util");
+                        double food = user.getDouble("food");
+                        double misc = user.getDouble("misc");
+                        double trans = user.getDouble("trans");
+                        double util = user.getDouble("util");
+
+                        drawChart(food, misc, trans, util);
 
                         /** FOUR DATABASE FIELDS GIVEN HERE, USE THIS TO PRESENT THE DATA */
 
@@ -78,9 +64,8 @@ public class HistoryActivity extends Activity {
                                 + ", " + Double.toString(trans[0]) + ", " + Double.toString(util[0]));
                         barchart.setDescription(description);*/
                     } else {
-                        Description description = new Description();
-                        description.setText("No user data found");
-                        barchart.setDescription(description);
+                        Toast.makeText(HistoryActivity.this, "No user data found!",
+                                Toast.LENGTH_SHORT).show();
                     }
 
                 }
@@ -88,52 +73,6 @@ public class HistoryActivity extends Activity {
         } else {
             Toast.makeText(this, "Nobody is signed in!", Toast.LENGTH_SHORT).show();
         }
-
-        /*int f = (int)food[0];
-        float t = (float)trans[0];
-        float u = (float)util[0];*/
-
-        /*ArrayList<BarEntry> barEntries = new ArrayList<>();
-        barEntries.add(new BarEntry(1, f));
-        barEntries.add(new BarEntry(2, t));
-        barEntries.add(new BarEntry(3, u));*/
-        ArrayList<BarEntry> barEntries = new ArrayList<>();
-        barEntries.add(new BarEntry(0, (float)food));
-        barEntries.add(new BarEntry(1, (float)misc));
-        barEntries.add(new BarEntry(2, (float)trans));
-        barEntries.add(new BarEntry(3, (float)util));
-
-        ArrayList<BarEntry> barEntries2 = new ArrayList<>();
-        barEntries2.add(new BarEntry(1, 400));
-        barEntries2.add(new BarEntry(2, 1000));
-        barEntries2.add(new BarEntry(3, 1000));
-
-        /*ArrayList<String>labels = new ArrayList<String>();
-        labels.add("Mar");
-        labels.add("Feb");
-        labels.add("Jan");*/
-
-        BarDataSet barDataSet = new BarDataSet(barEntries, "Actual Spending");
-        BarDataSet barDataSet2 = new BarDataSet(barEntries2, "Projected Spending");
-        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-        barDataSet2.setColors(ColorTemplate.COLORFUL_COLORS);
-
-        BarData data = new BarData(barDataSet, barDataSet2);
-        data.setBarWidth(0.35f);
-
-        barchart.setData(data);
-        barchart.groupBars(1, 0.1f, 0.02f);
-        //barchart.invalidate();
-        XAxis xAxis = barchart.getXAxis();
-        // String[] months =
-
-
-        //xAxis.setValueFormatter(new myXAxisValueFormatter(months));
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setGranularity(1);
-        xAxis.setCenterAxisLabels(true);
-        xAxis.setAxisMinimum(1);
-        onStop();
     }
 
 
@@ -175,6 +114,63 @@ public class HistoryActivity extends Activity {
         public String getFormattedValue(float value, AxisBase axis) {
             return mValues[(int) value];
         }
+    }
+
+    public void drawChart(double food, double misc, double trans, double util) {
+        BarChart barchart = (BarChart) findViewById(R.id.barchart);
+
+        barchart.setDrawBarShadow(false);
+        barchart.setDrawValueAboveBar(true);
+        barchart.setMaxVisibleValueCount(50);
+        barchart.setPinchZoom(false);
+        barchart.setDrawGridBackground(true);
+        barchart.setGridBackgroundColor(Color.TRANSPARENT);
+
+        final ArrayList<Double> vals = new ArrayList<>(Arrays.asList(0.0, 0.0, 0.0, 0.0));
+
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        barEntries.add(new BarEntry(0, (float)food)); //food
+        barEntries.add(new BarEntry(1, (float)misc)); //misc
+        barEntries.add(new BarEntry(2, (float)trans)); //trans
+        barEntries.add(new BarEntry(3, (float)util)); //util
+
+        ArrayList<BarEntry> barEntries2 = new ArrayList<>();
+        barEntries2.add(new BarEntry(1, 400));
+        barEntries2.add(new BarEntry(2, 1000));
+        barEntries2.add(new BarEntry(3, 1000));
+
+        Log.i("Food", Double.toString(food));
+        Log.i("Trans", Double.toString(misc));
+        Log.i("Util", Double.toString(trans));
+        Log.i("Misc", Double.toString(util));
+
+        /*ArrayList<String>labels = new ArrayList<String>();
+        labels.add("Mar");
+        labels.add("Feb");
+        labels.add("Jan");*/
+
+        BarDataSet barDataSet = new BarDataSet(barEntries, "Actual Spending");
+        BarDataSet barDataSet2 = new BarDataSet(barEntries2, "Projected Spending");
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        barDataSet2.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        BarData data = new BarData(barDataSet, barDataSet2);
+        data.setBarWidth(0.35f);
+
+        barchart.setData(data);
+        barchart.groupBars(1, 0.1f, 0.02f);
+        //barchart.invalidate();
+        XAxis xAxis = barchart.getXAxis();
+        // String[] months =
+
+
+        //xAxis.setValueFormatter(new myXAxisValueFormatter(months));
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setGranularity(1);
+        xAxis.setCenterAxisLabels(true);
+        xAxis.setAxisMinimum(1);
+
+        barchart.invalidate();
     }
 
     public void onStop() {
